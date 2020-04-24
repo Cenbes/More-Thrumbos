3a5d730f9186408c1dbc4ee3901114cac521385f
Cenbes <seleseth@gmail.com>
2020-04-24 04:45:30 +0200

    about/source files

---
+++ Source/Thrumbo/Thrumbo/PawnKindDefOf.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace Thrumbo
+{
+    [DefOf]
+    public static class PawnKindDefOf
+    {
+        static PawnKindDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(PawnKindDefOf));
+        }
+        public static PawnKindDef BlondThrumboPawn;
+        public static PawnKindDef GingerThrumboPawn;
+        public static PawnKindDef BionicThrumboPawn;
+        public static PawnKindDef BionicDamagedThrumboPawn;
+        public static PawnKindDef TalAlarianBlackThrumboPawn;
+        public static PawnKindDef TalAlarianSilverThrumboPawn;
+    }
+}
---
+++ Source/Thrumbo/Thrumbo/ThrumboList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace Thrumbo
+{
+    class ThrumboList
+    {
+        List<PawnKindDef> Thrumbos = New List<PawnKindDef>(){BlondThrumboPawn, GingerThrumboPawn, BionicThrumboPawn, BionicDamagedThrumboPawn, TalAlarianBlackThrumboPawn, TalAlarianSilverThrumboPawn};
+    }
+}
